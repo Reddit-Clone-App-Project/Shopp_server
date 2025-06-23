887d08de7b67c0b721e89b40b5c5a99a7173ce93
Matteo Conci <matteo.conci54@hotmail.it>
2025-06-24 00:44:05 +0200

    api adjustment for the frontend

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -1,24 +1,25 @@
 import pool from "../config/db";
-import { Store, StoreAddress, StoreOwner, StoreInfo, RatingStats, Review, StoreUpdate, StoreInfoUpdate } from "../types/store";
+import { StoreData, StoreAddress, StoreOwner, StoreInfo, RatingStats, Review, StoreUpdate, StoreInfoUpdate } from "../types/store";
+import { PoolClient } from "pg";
 
-export const createAddress = async (address: StoreAddress) => {
-    const result = await pool.query(
+export const createAddress = async (address: StoreAddress, client: PoolClient) => {
+    const result = await client.query(
         'INSERT INTO address (full_name, phone_number, country, province, city, postal_code, address_line1, address_line2) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
         [address.full_name, address.phone_number, address.country, address.province, address.city, address.postal_code, address.address_line1, address.address_line2]
     );
     return result.rows[0].id;
 };
 
-export const createStore = async (store: Store) => {
-    const result = await pool.query(
-        'INSERT INTO store (name, address_id, email, phone_number) VALUES ($1, $2, $3, $4) RETURNING id, name, email, phone_number',
-        [store.name, store.address_id, store.email, store.phone_number]
+export const createStore = async (store: StoreData, address_id: number, client: PoolClient) => {
+    const result = await client.query(
+        'INSERT INTO store (name, address_id, phone_number, email, express_shipping, fast_shipping, economical_shipping, bulky_shipping) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id, name, email, phone_number',
+        [store.storeName, address_id, store.storePhone, store.storeEmail, store.expressShipping, store.fastShipping, store.economicalShipping, store.bulkyShipping]
     );
     return result.rows[0];
 };
 
-export const createOwner = async (owner: StoreOwner ) => {
-    const result = await pool.query(
+export const createOwner = async (owner: StoreOwner, client: PoolClient) => {
+    const result = await client.query(
         'INSERT INTO store_user (store_id, app_user_id, role) VALUES ($1, $2, $3)',
         [owner.store_id, owner.app_user_id, owner.role]
     );
