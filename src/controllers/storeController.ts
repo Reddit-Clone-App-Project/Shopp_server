363c3b879960775dd078937bdc320cb1d8940e54
Nghiem Gia Bao <bao15022016@gmail.com>
2025-07-29 07:45:35 +0700

    Resolve conflict

--- src/controllers/storeController.ts
+++ src/controllers/storeController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import pool from '../config/db';
 import { StoreData, StoreOutput, StoreInfo, StoreAddress, RatingStats, Review, StoreInfoUpdate  } from '../types/store';
 import { createAddress, createStore, createOwner, getStores, getStoreProfile, getStoreAddressById, getRatingStats, getRecentReviews, updateStoreProfile, deleteStoreProfile, checkStoreOwner, getDiscountsByStoreId, getStoreTrendingProducts, getStoreProducts, getStoresOwned } from "../services/storeService";
+import { ProductCard } from "../types/product";
 
 export const registerStore = async (req: Request<{}, {}, StoreData>, res: Response) => {
     const data = req.body;
@@ -59,6 +60,29 @@ export const getAllStores = async ( req: Request, res: Response ) => {
         console.error("Error cannot get all stores", err);
         res.status(500).json({ error: "Error cannot get all stores" });
   };
+};
+
+export const getStoreByOwnerId = async ( req: Request, res: Response ) => {
+    
+    try{
+        if (req.user?.id === undefined) {
+            res.status(400).json({ error: 'User ID is required to get stores owned by user.' });
+            return;
+        };
+    
+        const userId = req.user?.id;
+        const storesOwned = await getStoresOwned(userId);
+        
+        if (!storesOwned || storesOwned.length === 0) {
+            res.status(404).json({ error: 'User is not the owner of any stores!'});
+            return;
+        }
+
+        res.status(200).json(storesOwned);
+    } catch (err) {
+        console.error('Error cannot get store by user id', err);
+        res.status(500).json({ error: 'Error cannot get store by user id'});
+    };
 };
 
 export const getStoreByOwnerId = async ( req: Request, res: Response ) => {
@@ -210,7 +234,7 @@ export const getStoreHotProducts = async ( req: Request, res: Response ) => {
     const limit = Number(req.query.limit) || 4;
     const offset = Number(req.query.offset) || 0;
     try {
-        const hotProducts = await getStoreTrendingProducts(storeId, limit, offset);
+        const hotProducts: ProductCard[] = await getStoreTrendingProducts(storeId, limit, offset);
         res.status(200).json(hotProducts);
     } catch (err) {
         console.error("Error cannot get store hot products", err);
@@ -223,7 +247,7 @@ export const getStoreProductsBought = async ( req: Request, res: Response ) => {
     const limit = Number(req.query.limit) || 5;
     const offset = Number(req.query.offset) || 0;
     try {
-        const products = await getStoreProducts(storeId, limit, offset);
+        const products: ProductCard[] = await getStoreProducts(storeId, limit, offset);
         res.status(200).json(products);
     } catch (err) {
         console.error("Error cannot get store products", err);
