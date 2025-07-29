7483a4cf321cf9067a6f5b3df3588371e1beb1d2
Nghiem Gia Bao <bao15022016@gmail.com>
2025-07-29 12:11:15 +0700

    Update the number of Limiter

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 
 export const globalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 5000,
     message: 'Too many requests, try again later',
     standardHeaders: true,
     legacyHeaders: false,
@@ -11,7 +11,7 @@ export const globalLimiter = rateLimit({
 
 export const authLimiter = rateLimit({
     windowMs: 5 * 60 * 1000,
-    max: 5,
+    max: 500,
     message: 'Too many requests, try again in 5 minutes',
     standardHeaders: true,
     legacyHeaders: false,
