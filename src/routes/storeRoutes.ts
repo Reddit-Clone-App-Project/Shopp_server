4adea45c95e8d5338b33ece75bf04191d701a878
Matteo Conci <matteo.conci54@hotmail.it>
2025-08-26 15:05:00 +0200

    AllProducts-patch (#38)
    * Add new controller and services for get All prodcuts from a store id
    
    * order routes
    
    * fix routes
    
    * Solve bug (#40)

--- src/routes/storeRoutes.ts
+++ src/routes/storeRoutes.ts
@@ -1,19 +1,20 @@
 import express from "express";
-import { registerStore, getStoreById, updateStore, deleteStore, getStoreHotProducts, getStoreDiscounts, getStoreProductsBought, getStoreByOwnerId } from "../controllers/storeController";
+import { registerStore, getStoreById, updateStore, deleteStore, getStoreHotProducts, getStoreDiscounts, getStoreProductsBought, getStoreByOwnerId, getStoreProductsById } from "../controllers/storeController";
 import { authenticateToken } from "../middlewares/authenticateToken";
 import { authLimiter } from "../middlewares/rateLimiter";
 
 const router = express.Router();
 
 router.post('/', authLimiter, authenticateToken, registerStore);
+router.get('/my-store', authenticateToken, getStoreByOwnerId);
 router.get('/:id', getStoreById);
-router.get('/my-store', authenticateToken, getStoreByOwnerId)
 router.put('/:id', authenticateToken, updateStore);
 router.delete('/:id', authenticateToken, deleteStore);
 
 // Store products
 router.get('/:id/products', getStoreProductsBought);
 router.get('/:id/products/hot', getStoreHotProducts);
+router.get('/:id/products/all', authenticateToken, getStoreProductsById);
 
 // Discount routes
 router.get('/:id/discounts', getStoreDiscounts);
